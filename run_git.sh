4c6e390ed8d534b9b397dfc8cdb9d39f97bb6e44
Gregory Bleiker <gregory.bleiker@zund.com>
2024-09-18 21:43:17 +0200

    script for git template

--- run_git.sh
+++ run_git.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clojure -Tnew create :template com.github.gregorybleiker/cljd-templates%dart-cli2%clojuredart/cli :name user/cli-app
+clojure -Tnew create :template com.github.gregorybleiker/cljd-templates%dart-cli%clojuredart/cli :name user/cli-app
